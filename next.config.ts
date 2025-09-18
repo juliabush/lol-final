e6d77255ae805d09cfa6aaacc46460905c23ef28
Thomas Bush <bushthomas@hotmail.com>
2025-09-18 18:35:39 +0200

    add 301s

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,47 @@ const nextConfig = {
   images: {
     domains: ['ddragon.leagueoflegends.com'],
   },
+  async redirects() {
+    return [
+      // Redirect old URLs to new ones
+      {
+        source: '/:locale/track',
+        destination: '/:locale/tracker',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/:locale/LookupID',
+        destination: '/:locale/',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/:locale/lists',
+        destination: '/:locale/',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/:locale/lists/:path*',
+        destination: '/:locale/',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/:locale/SearchAccountID/:path*',
+        destination: '/:locale/',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/:locale/generator/:param/Name Change/:name',
+        destination: '/:locale/generator/:name',
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/:locale/generator/:path*',
+        destination: '/:locale/generator',
+        permanent: true, // 301 redirect
+      },
+      // Add more redirects as needed
+    ]
+  },
 }
 
 const withNextIntl = createNextIntlPlugin();
