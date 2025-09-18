c5507c5a87a9ea351dab3de4ab1fa8fb1ed90e8b
Thomas Bush <bushthomas@hotmail.com>
2025-09-18 18:19:50 +0200

    fix

--- src/components/player-info.tsx
+++ src/components/player-info.tsx
@@ -303,7 +303,7 @@ export function PlayerInfo({ puuid, gameName, tagLine }: PlayerInfoProps) {
         <div className="relative w-20 h-20 rounded-full overflow-hidden border-3 border-blue-500">
           <Image 
             src={iconUrl}
-            alt={t('playerFound', { gameName: gameName || summonerInfo.name, tagLine: tagLine })}
+            alt={t('playerFound', { gameName: gameName || summonerInfo.name, tagLine: tagLine || '' })}
             fill
             className="object-cover"
           />
